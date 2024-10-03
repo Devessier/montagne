917475e9808a13cf572b8496eab7135d9c7ca6ff
Devessier <baptiste@devessier.fr>
2024-10-03 09:26:24 +0200

    feat: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,11 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), markdoc(), keystatic()],
   output: 'hybrid',
+  adapter: netlify(),
 });
