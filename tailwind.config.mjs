7ec16163c2cd2779ea122e7e387d40913fb00115
Devessier <baptiste@devessier.fr>
2024-11-13 12:52:24 +0100

    feat: set font family in tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: 'Avenir, Montserrat, Corbel, "URW Gothic", source-sans-pro, sans-serif'
+			},
 			borderWidth: {
 				'6': '6px'
 			}
