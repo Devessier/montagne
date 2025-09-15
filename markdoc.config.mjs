0935c65151e9ad052c68c75e1aa46175cb6ed195
Devessier <baptiste@devessier.fr>
2025-09-16 00:07:22 +0200

    feat: create video player addon for keystatic

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -1,10 +1,33 @@
 import { component, defineMarkdocConfig, nodes } from "@astrojs/markdoc/config";
 
 export default defineMarkdocConfig({
-    nodes: {
-        image: {
-            ...nodes.image,
-            render: component('./src/components/ImageContent.astro')
-        }
-    }
+  nodes: {
+    image: {
+      ...nodes.image,
+      render: component("./src/components/ImageContent.astro"),
+    },
+  },
+  tags: {
+    video: {
+      render: component("./src/components/VideoPlayer.tsx"),
+      attributes: {
+        src: {
+          type: String,
+          required: true,
+        },
+        controls: {
+          type: Boolean,
+          required: false,
+        },
+        autoplay: {
+          type: Boolean,
+          required: false,
+        },
+        loop: {
+          type: Boolean,
+          required: false,
+        },
+      },
+    },
+  },
 });
